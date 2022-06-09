270828151455a512b2069bd166558a18370da338
Michael Hirsch <scivision@users.noreply.github.com>
2022-06-09 17:39:10 -0400

    add sleep funciton

--- example/example_unix.f90
+++ example/example_unix.f90
@@ -1,6 +1,38 @@
+module sleep_std
+use, intrinsic :: iso_c_binding, only : c_int, c_long
+implicit none (type, external)
+
+private
+public :: sleep
+
+interface
+
+integer(c_int) function usleep(usec) bind (C)
+!! int usleep(useconds_t usec);
+!! https://linux.die.net/man/3/usleep
+import c_int
+integer(c_int), value, intent(in) :: usec
+end function usleep
+end interface
+
+contains
+
+subroutine sleep(millisec)
+integer, intent(in) :: millisec
+integer(c_int) :: ierr
+
+ierr = usleep(int(millisec * 1000, c_int))
+if (ierr/=0) error stop 'problem with usleep() system call'
+
+end subroutine sleep
+
+end module sleep_std
+
 program sigs
 
 use, intrinsic :: iso_c_binding, only: C_INT, C_CHAR
+
+use sleep_std, only : sleep
 
 implicit none (type, external)
 
