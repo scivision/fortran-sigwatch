54c8d432de82e677e64ee87d3097f9c03a79a679
scivision <scivision@users.noreply.github.com>
2023-11-04 22:24:50 -0400

    python: works on non-windows

--- test/test_sigwatch.py
+++ test/test_sigwatch.py
@@ -11,40 +11,51 @@ import os
 
 parser = argparse.ArgumentParser()
 parser.add_argument("exe", help="test executable")
-parser.add_argument("sleep", help="sleep time",
-                    nargs="?", type=float, default=0.5)
+parser.add_argument(
+    "sleep", help="sleep time", nargs="?", type=float, default=0.5
+)
 args = parser.parse_args()
 
 proc = subprocess.Popen(
     [args.exe],
-    shell=True,
+    shell=False,
+    creationflags=subprocess.CREATE_NEW_PROCESS_GROUP
+    if os.name == "nt"
+    else 0,
 )
-# creationflags=subprocess.CREATE_NEW_PROCESS_GROUP if os.name == "nt" else 0)
-# CREATE_NEW_PROCESS_GROUP  still didn't work -- no signals received
-# shell True or False didn't make a difference
+
+# not needed: preexec_fn=os.setsid if os.name != "nt" else None)
+# Windows: CREATE_NEW_PROCESS_GROUP  still didn't work --
+#     no signals received with shell True or False
+# macOS: works with shell True or False
+# Linux: works with shell False, but not True
 
 time.sleep(args.sleep)
 
+# print(, flush=True) needed to print at time of execution
+
 if os.name == "nt":
     try:
-        print("Python: send SIGBREAK", signal.SIGBREAK)  # type: ignore
+        print(
+            "Python: send SIGBREAK", signal.SIGBREAK, flush=True
+        )  # type: ignore
         proc.send_signal(signal.SIGBREAK)  # type: ignore
     except ValueError as e:
         print(f"Python failed: {e}")
 else:
-    print("Python: send SIGHUP", signal.SIGHUP)
-    proc.send_signal(signal.SIGHUP)
+    print("Python: send SIGHUP", signal.SIGHUP, flush=True)  # type: ignore
+    proc.send_signal(signal.SIGHUP)  # type: ignore
     time.sleep(args.sleep)
 
-    print("Python: send SIGINT", signal.SIGINT)
+    print("Python: send SIGINT", signal.SIGINT, flush=True)
     proc.send_signal(signal.SIGINT)
     time.sleep(args.sleep)
 
-    print("Python: send SIGUSR1", signal.SIGUSR1)  # type: ignore
+    print("Python: send SIGUSR1", signal.SIGUSR1, flush=True)  # type: ignore
     proc.send_signal(signal.SIGUSR1)  # type: ignore
 time.sleep(args.sleep)
 
-print("Python: send SIGTERM", signal.SIGTERM)
+print("Python: send SIGTERM", signal.SIGTERM, flush=True)
 proc.send_signal(signal.SIGTERM)
 time.sleep(args.sleep)
 
